de4bf1bfe435c85e95bb891c601a7d33925c9ded
Desi9nerd <swpark514@gmail.com>
2023-11-23 14:36:44 +0900

    Init

---
+++ Source/SWEditorTool/SWEditorToolGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SWEditorToolGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWEDITORTOOL_API ASWEditorToolGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
