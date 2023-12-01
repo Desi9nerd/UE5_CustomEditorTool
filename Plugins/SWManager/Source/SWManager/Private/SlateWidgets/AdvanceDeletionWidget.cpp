59954a318c036b882a444320181965bae141e06e
Desi9nerd <swpark514@gmail.com>
2023-12-01 17:41:33 +0900

    Slate UI - Slate Widget 텍스트 띄우기(글, 글자 크기, 중앙 정렬, 글씨 색 수정)

--- Plugins/SWManager/Source/SWManager/Private/SlateWidgets/AdvanceDeletionWidget.cpp
+++ Plugins/SWManager/Source/SWManager/Private/SlateWidgets/AdvanceDeletionWidget.cpp
@@ -1,13 +1,28 @@
-#include "SlateWidgets/AdvanceDeletionWidget.h"
+﻿#include "SlateWidgets/AdvanceDeletionWidget.h"
 
 void SAdvanceDeletionTab::Construct(const FArguments& InArgs)
 {
 	bCanSupportFocus = true;
-	
+
+	FSlateFontInfo TitleTextFont = FCoreStyle::Get().GetFontStyle(FName("EmbossedText")); // 글꼴
+	TitleTextFont.Size = 30; // 글자 크기
+
 	ChildSlot
-	[
-		SNew(STextBlock)
-		.Text(FText::FromString(InArgs._TestString))
+	[		
+		SNew(SVerticalBox) // Main vertical box
+
+		// First vertical slot for title text
+		+ SVerticalBox::Slot()
+		.AutoHeight()
+		[
+			SNew(STextBlock)
+			.Text(FText::FromString(TEXT("제거 마법사")))
+			.Font(TitleTextFont) // 글꼴, 글자 크기 설정
+			.Justification(ETextJustify::Center) // 글자 중앙 정렬
+			.ColorAndOpacity(FColor::White)
+		]
+
+
 	];
 
 }
