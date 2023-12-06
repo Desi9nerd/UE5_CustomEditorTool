c6e9e826e43c85c502b13d794ec9758f7ccdf8fd
Desi9nerd <swpark514@gmail.com>
2023-12-07 01:23:35 +0900

    Slate UI - ShutdownModule()

--- Plugins/SWManager/Source/SWManager/Private/SWManager.cpp
+++ Plugins/SWManager/Source/SWManager/Private/SWManager.cpp
@@ -364,6 +364,7 @@ void FSWManagerModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+	FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(FName("AdvanceDeletion")); // Tab ID: "AdvanceDeletion" 
 }
 
 #undef LOCTEXT_NAMESPACE
